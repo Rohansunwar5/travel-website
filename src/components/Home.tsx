0273edaf9781e596442923819579c92341437223
Rohan Sunwar <rohansunwar5@gmail.com>
2024-11-17 00:17:22 +0530

    fixed

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -55,13 +55,14 @@ const Hero = () => {
         style={{ marginTop: '0px' }}
       >
        <motion.h3
-        className="text-[#4B4376] py-2 relative after:content-[&quot;&quot;] after:w-[83%] after:h-[2px] after:bg-[#4B4376] after:right-0 after:bottom-1/2 after:absolute"
+        className="text-[#4B4376] py-2 relative after:content-[''] after:w-[83%] after:h-[2px] after:bg-[#4B4376] after:right-0 after:bottom-1/2 after:absolute"
         initial={{ opacity: 0, x: -50 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 1, ease: 'easeInOut', delay: 1 }}
         >
         Popular Places
         </motion.h3>
+
 
         <motion.div
           className="items-center justify-center gap-8 flex"
