09510d5b9f3e9d8ea42f938c86ee2b3190e8f8f5
Rohan Sunwar <rohansunwar5@gmail.com>
2024-11-16 20:48:51 +0530

    fixed page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import Cards from "@/components/Cards";
 import Features from "@/components/Features";
-import HeroSection from "@/components/Hero";
 import Hero from '@/components/Home';
 
 
@@ -10,7 +9,6 @@ export default function Home() {
   return (
     <div className="bg-[#4B4376]">
       <Hero/>
-     {/* <HeroSection/> */}
      <Cards/>
      <Features/>
     </div>
